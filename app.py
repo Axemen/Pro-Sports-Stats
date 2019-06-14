a3a577ff941306fea1ae5ebbc48e3b855b192e37
Devynn Moreno <devynn.moreno@gmail.com>
2019-06-13 19:07:27 -0500

    setting up heroku

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ import os
 # from libs import scraper
 
 ##detect your environment, and conditionally load config file
-if not os.environ.get('DYNO'):
-    import config
-    print(config.name)
-
-##detect your environment, and conditionally load database url
-if os.environ.get("JAWSDB_URL"):
-    dburl = os.environ["JAWSDB_URL"]
-else:
-    dburl = config.dburl
-
-# create an engine for your database connection
-engine = sqlalchemy.create_engine(dburl)
-
-# Use pandas to read in a sql table
-df = pandas.read_sql("SELECT * FROM TABLENAME_HERE", engine)
-
+# if not os.environ.get('DYNO'):
+#     import config
+#     print(config.name)
+# 
+# ##detect your environment, and conditionally load database url
+# if os.environ.get("JAWSDB_URL"):
+#     dburl = os.environ["JAWSDB_URL"]
+# else:
+#     dburl = config.dburl
+# 
+# # create an engine for your database connection
+# engine = sqlalchemy.create_engine(dburl)
+# 
+# # Use pandas to read in a sql table
+# df = pandas.read_sql("SELECT * FROM TABLENAME_HERE", engine)
+# 
 
 app = Flask(__name__)
 
@@ -30,11 +30,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-
-# respond with JSON
-@app.route("/data")
-def data():
-    return jsonify(df.to_json(orient="records"))
+# 
+# # respond with JSON
+# @app.route("/data")
+# def data():
+#     return jsonify(df.to_json(orient="records"))
 
 # @app.route("/scrape")
 # def scrape():
