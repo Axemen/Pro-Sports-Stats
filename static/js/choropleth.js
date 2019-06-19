31847d4623fea7da32234738c92ba3becfd04ab6
Jrandolph011@gmail.com <Jrandolph011@gmail.com>
2019-06-19 05:16:31 -0500

    choropleth start

--- static/js/choropleth.js
+++ static/js/choropleth.js
@@ -1,91 +1,110 @@
-const STATESURL = 'https://leafletjs.com/examples/choropleth/us-states.js';
+d3.json('static/data/us-states.json').then(statesData => {
 
-const map = L.map('map').setView([37.8, -96], 4);
+    d3.csv('static/data/year_state_av_avg.csv').then(warData => {
 
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>, <br> Created by Jonathan Randolph",
-    maxZoom: 18,
-    id: "mapbox.light",
-    accessToken: MAPBOX_KEY
-}).addTo(map);
+        warData.forEach(d => {
+            statesData.features.forEach(f => {
+                if(d.state === f.properties.abbr){
+                    f.properties.avg = d.war
+                }
+            });
+        });
 
-L.geoJson(statesData).addTo(map);
+        statesData.features.forEach(f =>{
+            warData.forEach(d => {
+                if(d.Year === '2015'){
+                    if(d.state === f.properties.abbr){
+                        f.properties.war = d.av
+                        // console.log(f.properties.war);
+                    }
+                }
+            });
+        });
 
-function getColor(d) {
-    return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
-}
+        const map = L.map('map').setView([37.8, -96], 4);
 
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.density),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
+        L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>, <br> Created by Jonathan Randolph",
+            maxZoom: 18,
+            id: "mapbox.light",
+            accessToken: MAPBOX_KEY
+        }).addTo(map);
 
-var geojson;
+        L.geoJson(statesData).addTo(map);
 
-function highlightFeature(e) {
-    var layer = e.target;
+        let smList = warData.map(d => d.av);
 
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
+        var scale = d3.scaleLinear()
+            .range([d3.rgb(94, 16, 249), d3.rgb(214, 245, 0), d3.rgb(213, 222, 217)])
+            .domain([0, 4 ,7.5]);
+
+        function style(feature) {
+            // console.log(feature.properties.av)
+            return {
+                fillColor: scale(feature.properties.war), //colorScale(feature.properties.av),
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7
+            };
+        }
+
+        var geojson;
+
+        function highlightFeature(e) {
+            var layer = e.target;
+
+            layer.setStyle({
+                weight: 5,
+                color: '#666',
+                dashArray: '',
+                fillOpacity: 0.7
+            });
+
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+            }
+            info.update(layer.feature.properties);
+        }
+
+        function resetHighlight(e) {
+            geojson.resetStyle(e.target);
+            info.update();
+        }
+
+        function zoomToFeature(e) {
+            map.fitBounds(e.target.getBounds());
+        }
+
+        function onEachFeature(feature, layer) {
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            });
+        }
+
+        geojson = L.geoJson(statesData, {
+            style: style,
+            onEachFeature: onEachFeature
+        }).addTo(map);
+
+        var info = L.control();
+
+        info.onAdd = function (map) {
+            this._div = L.DomUtil.create('div', 'info');
+            this.update();
+            return this._div;
+        };
+
+        info.update = function (props) {
+            this._div.innerHTML = '<h4> Average War Rating Per state</h4>' + (props ?
+                '<b>' + props.abbr + '</b><br />' + props.war + ' War'
+                : 'Hover over a state');
+            
+        };
+
+        info.addTo(map);
     });
-
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
-    info.update(layer.feature.properties);
-}
-
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
-}
-
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
-
-geojson = L.geoJson(statesData, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-var info = L.control();
-
-info.onAdd = function(map) {
-    this._div = L.DomUtil.create('div', 'info');
-    this.update();
-    return this._div;
-};
-
-info.update = function (props) {
-    this._div.innerHTML = '<h4> US Population Density</h4>' + (props ?
-        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-        : 'Hover over a state');
-};
-
-info.addTo(map);
-
+});
