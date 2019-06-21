e906c53c6285fe320cf148f342ec663ac834f5ad
logan7734 <logan7734@gmail.com>
2019-06-20 19:37:57 -0500

    fixed fast facts file

--- static/js/infographics.js
+++ static/js/infographics.js
@@ -1,70 +1,38 @@
 function buildFacts(newstate){
 
-    console.log(newstate);
+    //console.log(newstate);
     var panelnfl = d3.select("#nflcard");
     panelnfl.html("");
 
-    d3.csv("../static/data/nfl_player_av_state.csv").then((nflplayers) => {
-    var filteredData = nflplayers.filter(nflplayers => nflplayers.state === newstate.Abbr);
+    d3.csv("/libs/unclean_data/nfl_fast_facts_data.csv").then((nflplayers) => {
+    var filteredDatanfl = nflplayers.filter(nflplayers => nflplayers.state === newstate);
+
+    var sorted_av_nfl = filteredDatanfl.sort(function(first, second) {
+      return second.AV - first.AV;
+  
+     }); 
+
+     Object.entries(sorted_av_nfl[0]).forEach(([key, value])=> {
+       panelnfl.append("h6").text(`${key}:${value}`);
+     });
+    });
+
+     var panelmlb = d3.select("#mlbcard");
+     panelmlb.html("");
+ 
+     d3.csv("/libs/unclean_data/mlb_fast_facts_data.csv").then((mlbplayers) => {
+     var filteredDatamlb = mlbplayers.filter(mlbplayers => mlbplayers.state === newstate);
+ 
+     var sorted_av_mlb = filteredDatamlb.sort(function(first, second) {
+       return second.WAR - first.WAR;
    
-    //panelnfl.append("h6").text(filteredData.Player);
-    // filteredData.forEach((statedata) => {
-
-    //     Object.values(statedata).forEach((value) => {
-
-    //         row.append("td").text(value);
-            
-    //     })
-    // }); 
-        // var myKeys = Object.keys(nflplayers).filter(state => state == newstate);
-        // console.log(myKeys);
-        // let result = myKeys.reduce((r, k) => r.concat(data[k]), []);
-        // console.log(result);
-        // console.log(nflplayers);
-        // console.log(newstate.Abbr);
-        // var filtered = nflplayers.filter(function(d) {
-        //     if( d["state"] == newstate.Abbr) 
-        //     { 
-        //         return d;
-        //     } 
-        //   });
-        // console.log(filtered);
-        // // nflplayers.filter(function(d) {
-        // //     return d["state"] === newstate;
-        // // });
-        // console.log(newstate.Abbr);
-        // filtered.forEach((players) => {
-            
-        //     // if( players["state"] == newstate.Abbr) 
-        //     // {
-        //         panelnfl.append("h6").text(players.Player);
-        //     // }
-           
-        // });
-        // console.log('Not filtered', Object.entries(nflplayers));
-        // var myresult = Object.entries(nflplayers).filter((item) => (item.indexOf('state')> -1))
-        // console.log(myresult);
-        // .forEach(([key, value]) => {
-        //     console.log(`${key}:${value}`);
-        //     panelnfl.append("h6").text(`${key}:${value}`);
-        // });
+      }); 
+ 
+      Object.entries(sorted_av_mlb[0]).forEach(([key, value])=> {
+        panelmlb.append("h6").text(`${key}:${value}`);
+      });
     });
     
-//     var url = `metadata/${sample}`;
-//     // Use `d3.json` to fetch the metadata for a sample
-//   d3.json(url).then(function (metadata) {
-//     console.log(metadata);
-//  // Use d3 to select the panel with id of `#sample-metadata`
-//     var panel = d3.select("#sample-metadata");
-//  // Use `.html("") to clear any existing metadata
-//     panel.html("");
-//   // Use `Object.entries` to add each key and value pair to the panel
-//     Object.entries(metadata).forEach(([key, value]) => {
-//     // Hint: Inside the loop, you will need to use d3 to append new
-//     // tags for each key-value in the metadata.
-//       panel.append("h6").text(`${key}:${value}`);
-//     });
-// });
 }
 
 
@@ -83,15 +51,14 @@ function init() {
         });
   
       // Use the first sample from the list to build the initial plots
-      const firstState = statenames[0];
-      console.log(firstState);
-        buildFacts(firstState);
+      const firstState = statenames[0]; console.log(firstState);
+        buildFacts(firstState.Abbr);
     });
   }
   
   function optionChanged(newState) {
     // Fetch new data each time a new sample is selected
-        buildFacts(newState);
+        buildFacts(newState); console.log(newState);
   }
   
   // Initialize the dashboard
